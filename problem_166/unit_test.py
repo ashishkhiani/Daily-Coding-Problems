edb41847daea715bcb901535c8512bbf25904e6f
Ashish Khiani <ak_khiani95@hotmail.com>
2018-09-28 10:49:46 -0400

    Tests for exceptions raised

--- problem_166/unit_test.py
+++ problem_166/unit_test.py
@@ -16,3 +16,13 @@ class IteratorTest(unittest.TestCase):
             actual.append(iterator.next())
 
         self.assertEqual(expected, actual)
+
+    def test_exception_raised(self):
+        _input = [[1, 2], [3], [], [4, 5, 6]]
+        iterator = Iterator(_input)
+
+        while iterator.has_next():
+            iterator.next()
+
+        with self.assertRaises(StopIteration):
+            iterator.next()
