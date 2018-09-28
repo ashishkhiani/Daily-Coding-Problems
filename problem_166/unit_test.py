614d4e0e269fb75709df67a38310e2c8d603d5b0
Ashish Khiani <ak_khiani95@hotmail.com>
2018-09-28 11:14:25 -0400

    Added more test cases

--- problem_166/unit_test.py
+++ problem_166/unit_test.py
@@ -6,6 +6,7 @@ from problem_166.solution import Iterator
 class IteratorTest(unittest.TestCase):
 
     def test_example(self):
+        """Tests the given example"""
         _input = [[1, 2], [3], [], [4, 5, 6]]
         iterator = Iterator(_input)
 
@@ -18,11 +19,47 @@ class IteratorTest(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_exception_raised(self):
+        """Tests the case where next() is called when there are no more elements"""
         _input = [[1, 2], [3], [], [4, 5, 6]]
         iterator = Iterator(_input)
 
         while iterator.has_next():
             iterator.next()
 
+        self.assertFalse(iterator.has_next())
+
         with self.assertRaises(StopIteration):
             iterator.next()
+
+    def test_empty_input(self):
+        """Tests the case when the input given is an empty array"""
+        _input = []
+        iterator = Iterator(_input)
+
+        self.assertFalse(iterator.has_next())
+
+        with self.assertRaises(StopIteration):
+            iterator.next()
+
+    def test_double_empty_input(self):
+        """Tests the case where the input is an array containing an empty array -> [[]]"""
+
+        _input = [[]]
+        iterator = Iterator(_input)
+
+        self.assertFalse(iterator.has_next())
+
+        with self.assertRaises(StopIteration):
+            iterator.next()
+
+    def test_input_with_empty_array(self):
+        """Tests the case when there is an empty array with other arrays"""
+        _input = [[], [1], [2], [3, 4]]
+
+        iterator = Iterator(_input)
+
+        self.assertEqual(iterator.next(), 1)
+        self.assertEqual(iterator.next(), 2)
+        self.assertEqual(iterator.next(), 3)
+        self.assertEqual(iterator.next(), 4)
+
