1c5ac843a20f7d51bbaf145f0555dcb9d77c6569
Ashish Khiani <ak_khiani95@hotmail.com>
2018-09-28 10:38:09 -0400

    Completed solution to problem_166

---
+++ problem_166/unit_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from problem_166.solution import Iterator
+
+
+class IteratorTest(unittest.TestCase):
+
+    def test_example(self):
+        _input = [[1, 2], [3], [], [4, 5, 6]]
+        iterator = Iterator(_input)
+
+        expected = [1, 2, 3, 4, 5, 6]
+        actual = []
+
+        while iterator.has_next():
+            actual.append(iterator.next())
+
+        self.assertEqual(expected, actual)
